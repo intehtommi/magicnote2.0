5e65659b6a39564163fb8c72847834730c2693e3
leaftommi <inthetommi@KentonoMacBook-Pro.local>
2020-12-03 23:08:57 +0900

    first commit. A simple button to stop a question when spawning

---
+++ Assets/SpawnText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KoganeUnityLib;
+public class SpawnText : MonoBehaviour
+{
+    TMP_Typewriter m_typewriter;
+    float m_speed = 10F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_typewriter = gameObject.GetComponent<TMP_Typewriter>();
+        m_typewriter.Play
+        (
+            text        : "めっちゃ強いクイズプレイヤーは誰？",
+            speed       : m_speed,
+            onComplete  : () => Debug.Log( "完了" )
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            m_typewriter.Pause();
+        }
+    }
+}
